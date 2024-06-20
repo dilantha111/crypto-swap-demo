50b02f0f03bfcb8999f6afd278ae5d1b92279d3a
Dilantha Wijayarathne <dwijayarathne@palo-it.com>
2024-06-20 17:22:06 +0800

    add simple placeholder navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import ConnectWalletButton from "./components/ConnectWalletButton";
+import Navigation from "./components/Navigation";
 import SwapComponent from "./components/SwapComponent";
 import { TokenContext } from "./contexts/TokenContext";
 import { getTokens } from "./services/token.service";
@@ -17,6 +18,8 @@ function App() {
 
   return (
     <div className='App'>
+      <Navigation />
+
       <TokenContext.Provider value={tokens}>
         <h1 className='main-title'>Swap anytime, anywhere</h1>
         <div className='main-container'>
